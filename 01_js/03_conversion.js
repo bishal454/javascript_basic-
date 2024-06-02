be4279889e6ae3c56e3aa12423ffbfda9c01cc24
bishal454 <166239531+bishal454@users.noreply.github.com>
2024-06-02 17:30:24 +0000

    conversion of datatypes

---
+++ 01_js/03_conversion.js
@@ -0,0 +1,12 @@
+let score="33a"
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+
+let result=1
+let resultInBoolean=Boolean(result)
+console.log(typeof(resultInBoolean))
+console.log(resultInBoolean)
+
+
