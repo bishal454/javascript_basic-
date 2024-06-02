a5fdfa03ee432d3dfcbfc6317c1d79ad04449b50
bishal454 <166239531+bishal454@users.noreply.github.com>
2024-06-02 15:57:58 +0000

    container config done

---
+++ 01_js/01_variables.js
@@ -0,0 +1,6 @@
+const accountId=12
+let accountEmail ="bishal@gmail.com";
+var accountPassword="biswe12@"
+accountcity="janakpur"
+ let accountstate;
+console.table([accountId,accountEmail,accountcity,accountPassword,accountstate])
