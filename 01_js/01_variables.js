dfdedf8a80b6fd02dc1a4e5b13289a53e1bf1bf9
bishal454 <166239531+bishal454@users.noreply.github.com>
2024-06-02 17:15:57 +0000

    variable in js

--- 01_js/01_variables.js
+++ 01_js/01_variables.js
@@ -1,6 +1,6 @@
 const accountId=12
 let accountEmail ="bishal@gmail.com";
-var accountPassword="biswe12@"
+var accountPassword="biswe12@"//we will not use var bcz of issue of block  scope and functional scope 
 accountcity="janakpur"
  let accountstate;
 console.table([accountId,accountEmail,accountcity,accountPassword,accountstate])
