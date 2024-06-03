84aaaf3244bc4f47c489444b2aff2283d4a43b98
bishal454 <166239531+bishal454@users.noreply.github.com>
2024-06-03 11:40:36 +0000

    Call by reference data type

---
+++ 01_js/06_nonPrimitive.js
@@ -0,0 +1,23 @@
+// array
+
+let heros=["ironman", "thor","saktiman","doga"]
+
+// object datatype
+let myObj={
+    firstName:"Bishal",
+    lastName:"sah",
+    age:22,
+    gender:true,
+
+}
+
+const  myFunction=Function()
+{
+    
+    console.log("hello World")
+}
+
+console.log(typeof myFunction)
+console.log(typeof myObj)
+console.log(typeof heros)
+// console.log(typeof myFunction)
