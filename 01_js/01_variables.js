f8dfc657c4ab643abc37a7ac95d67e617408ec7c
bishal454 <166239531+bishal454@users.noreply.github.com>
2024-06-03 17:53:27 +0000

    different types of variable in js

--- 01_js/01_variables.js
+++
@@ -1,6 +0,0 @@
-const accountId=12
-let accountEmail ="bishal@gmail.com";
-var accountPassword="biswe12@"//we will not use var bcz of issue of block  scope and functional scope 
-accountcity="janakpur"
- let accountstate;
-console.table([accountId,accountEmail,accountcity,accountPassword,accountstate])
