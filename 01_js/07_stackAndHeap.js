b2938c8ca7b098f8a5e14600bea655ae6a22aa59
bishal454 <166239531+bishal454@users.noreply.github.com>
2024-06-03 12:36:47 +0000

    Memory  layout  in js

---
+++ 01_js/07_stackAndHeap.js
@@ -0,0 +1,31 @@
+
+// premitive data type 
+let firstName="Bishal"
+let anotherName=firstName
+console.log(firstName)
+console.log(anotherName)
+
+anotherName="Ranjan"
+
+console.log(firstName)
+console.log(anotherName)
+
+
+
+// non-primitive data type 
+
+
+let user1={
+    email:"user@gmail.com",
+    country:"india",
+    upiId:"922222@bily",
+
+}
+
+let user2=user1
+console.log(user1)
+console.log(user2)
+user2.country="Nepal"
+
+console.log(user1)
+console.log(user2)
