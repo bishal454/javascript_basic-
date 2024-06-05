287be6091dc2b6890b4b5501c8931c623c2457cf
bishal454 <166239531+bishal454@users.noreply.github.com>
2024-06-05 12:07:54 +0000

    onject with api

---
+++ 02basic/05_objAndApi.js
@@ -0,0 +1,15 @@
+let course={
+    courseName:"js for web dev",
+    price:299,
+    courseInstructure:"Hitesh"
+}
+const {courseInstructure : instruct }=course
+console.log(instruct)
+
+
+// JOSON API
+
+{
+    "name":"bisal",
+    "email":"bishal@.com",
+}
