5931aaa1bbdfa5f6ef1bc0e51abceb5360cef031
bishal454 <166239531+bishal454@users.noreply.github.com>
2024-06-05 18:21:11 +0000

    basic about the function

--- 03_basic /01_funstion.js
+++ 03_basic /01_funstion.js
@@ -1,8 +1,9 @@
-function addTwoNumbers(num1,num2){
+function addTwoNumbers(num1,num2)// parameter
+{
     // console.log(num1+num2)
     return num1+num2
 }
-  const result=addTwoNumbers(3,2)
+  const result=addTwoNumbers(3,2)// argument 
   console.log(`result is ${result}`)
 
   function  loggedIn(usernName="bis"){
