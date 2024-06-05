8bae8fe2cd670c4927c10adcea48ac3efae0108b
bishal454 <166239531+bishal454@users.noreply.github.com>
2024-06-05 12:31:11 +0000

    basic about function in js

--- 03_basic /01_funstion.js
+++ 03_basic /01_funstion.js
@@ -1,1 +1,17 @@
-funs
+function addTwoNumbers(num1,num2){
+    // console.log(num1+num2)
+    return num1+num2
+}
+  const result=addTwoNumbers(3,2)
+  console.log(`result is ${result}`)
+
+  function  loggedIn(usernName="bis"){
+    if(!usernName){
+        console.log(`Please enter  name`)
+        return 
+    }
+    
+    return  `${usernName} just logged inside the website `
+}
+let view =loggedIn()
+console.log(view)
