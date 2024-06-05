dfd70580873f7f483fd696b094ad9874c837f8e3
bishal454 <166239531+bishal454@users.noreply.github.com>
2024-06-05 19:10:24 +0000

    switch case in js

---
+++ 04_controlFlow.js/02_switch.js
@@ -0,0 +1,27 @@
+const month =3
+switch(month){
+    case 1:
+        console.log("Janauary");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    default:
+        console.log("Invalid");
+        break;
+}
