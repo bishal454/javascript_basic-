540ea63dc85975cdbf5d377460adcb6c5b21ab9f
bishal454 <166239531+bishal454@users.noreply.github.com>
2024-06-08 10:25:10 +0000

    for loop

---
+++ 05_Iterations.js/01_forLoop.js
@@ -0,0 +1,29 @@
+// for(let value=0;value<=10;value++)
+//     {
+//     console.log(value)
+// }
+//  for(let i=1;i<=20;i++){
+//     console.log(`the  table  of: ${i} is given below`)
+//     for(let j=0;j<=10;j++){
+//         // console.log(`the inner loop is: ${j} and outer loop is: ${i}`)
+//         console.log(i + '*' + j + '=' + i*j)
+//     }
+//  }
+for (let i = 1; i <20; i++) {
+    if(i==5){
+        console.log(`detected 5`)
+        break
+
+    }
+    console.log(`the value of i is :${i}`)
+    
+}
+for (let i = 1; i <20; i++) {
+    if(i==5){
+        console.log(`detected 5`)
+        continue
+
+    }
+    console.log(`the value of i is :${i}`)
+    
+}
