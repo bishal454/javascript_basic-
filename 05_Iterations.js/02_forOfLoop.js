5b4975de0ed13e198a552af120d168a311641524
bishal454 <166239531+bishal454@users.noreply.github.com>
2024-06-08 11:30:34 +0000

    for of loop i js for espically array

---
+++ 05_Iterations.js/02_forOfLoop.js
@@ -0,0 +1,11 @@
+let myArr=[1,2,3,4,5,6,7,8]
+for (const greet of myArr) {
+    console.log(greet)
+    
+}
+console.log()
+const greeting="Hello World!"
+for (const greet of  greeting) {
+    console.log(greet)
+    
+}
